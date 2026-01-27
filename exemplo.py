b15ba3fe292d29c35361ff6a4a2c52dd2d197170
Felipe Vital <felipezreal@gmail.com>
2026-01-27 20:52:45 -0300

    add log

--- exemplo.py
+++ exemplo.py
@@ -1,3 +1,10 @@
-def soma(x,y) -> int:
+from loguru import logger
+
+logger.add("meu_app.log")
+
+def soma(x,y):
+    logger.info(x)
+    logger.info(y)
+    logger.info(x+y)
     return x + y
-print(soma(4,1))
+soma(2,"3")
